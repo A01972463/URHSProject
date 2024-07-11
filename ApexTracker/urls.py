2a2ab96eb21476032c89efce8fd7fa0bb8e0cbd2
Gustavo <gustavohuerta96@gmail.com>
2024-07-11 11:07:27 -0600

    Added main pages

--- ApexTracker/urls.py
+++ ApexTracker/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'ApexTracker'
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('input/', views.input, name='input'),
+    path('report/', views.report, name='report'),
+    path('settings/', views.settings, name='settings')
 ]
--- ApexTracker/views.py
+++ ApexTracker/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Apex Tracker index.")
+    return render(request, 'ApexTracker/index.html')
+
+def input(request):
+    return render(request, 'ApexTracker/input.html')
+
+def report(request):
+    return render(request, 'ApexTracker/report.html')
+
+def settings(request):
+    return render(request, 'ApexTracker/settings.html')
